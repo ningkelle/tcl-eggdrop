08be399ecee142381dcf81358efdd77f21b7a840
Ningkelle <ningkelle@gmail.com>
2023-10-20 15:57:00 +0700

    Update youtubesearch.tcl

--- youtubesearch.tcl
+++ youtubesearch.tcl
@@ -2,9 +2,8 @@ bind pub - .yt pub:ytsearch
 proc pub:ytsearch {n u h c t} {
  set t [stripcodes bcruag $t]
  if {$t == ""} {putnow "privmsg $c :Usage: .yt <keyword>" ; return 0}
- regsub -all {\s+} $t "%20" text
  set apikey "1234567890ABCDEFGHIJKLMNOPQRSTUVWQYZ" ; #YouTube Data API v3 : https://developers.google.com/youtube/v3/getting-started
- if {[catch {set ytpage [http::geturl "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&regionCode=id&safeSearch=none&type=video&key=$apikey&q=$text" -timeout 60000]} error]} {putnow "privmsg $c :$error" ; return}
+ if {[catch {set ytpage [http::geturl "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&regionCode=id&safeSearch=none&type=video&key=$apikey&[http::formatQuery q $text]" -timeout 60000]} error]} {putnow "privmsg $c :$error" ; return}
  set response [::json::json2dict [http::data $ytpage]] ; http::cleanup $ytpage
  foreach var {pageInfo items} {set $var [dict get $response $var]} ; set totalResults [dict get $pageInfo totalResults]
  if {$totalResults == 0} {putnow "privmsg $c :\037YouTube:\037 \00359$t\003 \0034»»\003 hasil tidak ditemukan" ; return} else {putnow "privmsg $c :\037YouTube:\037 \00359$t\003 \0034»»\003 \002$totalResults\002 hasil ditemukan"}
