717a12bf7bdd6907227438b1b9006410b3ab4065
Ningkelle <ningkelle@gmail.com>
2023-10-20 15:36:03 +0700

    Update youtubesearch.tcl

--- youtubesearch.tcl
+++ youtubesearch.tcl
@@ -6,7 +6,6 @@ proc pub:ytsearch {n u h c t} {
  set apikey "1234567890ABCDEFGHIJKLMNOPQRSTUVWQYZ" ; #YouTube Data API v3 : https://developers.google.com/youtube/v3/getting-started
  if {[catch {set ytpage [http::geturl "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&regionCode=id&safeSearch=none&type=video&key=$apikey&q=$text" -timeout 60000]} error]} {putnow "privmsg $c :$error" ; return}
  set response [::json::json2dict [http::data $ytpage]] ; http::cleanup $ytpage
- if {![dict exists $response pageInfo]} {putnow "privmsg $c :\037YouTube:\037 \0034»»\003 Error!" ; return}
  foreach var {pageInfo items} {set $var [dict get $response $var]} ; set totalResults [dict get $pageInfo totalResults]
  if {$totalResults == 0} {putnow "privmsg $c :\037YouTube:\037 \00359$t\003 \0034»»\003 hasil tidak ditemukan" ; return} else {putnow "privmsg $c :\037YouTube:\037 \00359$t\003 \0034»»\003 \002$totalResults\002 hasil ditemukan"}
  foreach line $items {if {[dict exists [dict get $line id] videoId]} {set videoid [dict get [dict get $line id] videoId]} ; set title [dict get [dict get $line snippet] title] ; set chtitle [dict get [dict get $line snippet] channelTitle] ; set items "\nhttps://youtu.be/$videoid \0034»»\003 $title \0034-\003 \00353by\003 $chtitle" ; append result $items}
