2999d5882847cb6e7554b1dcb0ce5214fc9b81b3
Ningkelle <ningkelle@gmail.com>
2023-10-25 16:57:40 +0700

    Update infogempa.tcl

--- infogempa.tcl
+++ infogempa.tcl
@@ -1,10 +1,10 @@
 bind pub - .gempa pub:gempa
 proc pub:gempa {n u h c t} {
- if {[catch {set gempaurl [http::geturl https://cuaca-gempa-rest-api.vercel.app/quake -timeout 3000]} error]} {putnow "privmsg $c :$error" ; return}
+ if {[catch {set gempaurl [http::geturl https://data.bmkg.go.id/DataMKG/TEWS/autogempa.json -timeout 3000]} error]} {putnow "privmsg $c :$error" ; return}
  set response [::json::json2dict [http::data $gempaurl]] ; http::cleanup $gempaurl ; putlog "$response"
- set gempa [dict get $response data] ; foreach data {tanggal jam lintang bujur magnitude kedalaman wilayah potensi dirasakan shakemap} {set $data [dict get $gempa $data]}
- putnow "privmsg $c :\037Lokasi:\037 $wilayah \($lintang\, $bujur\) \0034-\003 $dirasakan"
- putnow "privmsg $c :\037Skala:\037 $magnitude Magnitudo \0034-\003 \037Kedalaman:\037 $kedalaman \0034-\003 \037Waktu:\037 $tanggal $jam \0034-\003 \037Potensi:\037 $potensi"
- putnow "privmsg $c :\037$shakemap\037"
+ set gempa [dict get $response Infogempa gempa] ; foreach data {Tanggal Jam Lintang Bujur Magnitude Kedalaman Wilayah Potensi Dirasakan Shakemap} {set $data [dict get $gempa $data]}
+ putnow "privmsg $c :\037Lokasi:\037 $Wilayah \($Lintang\, $Bujur\) \0034-\003 $Dirasakan"
+ putnow "privmsg $c :\037Skala:\037 $Magnitude Magnitudo \0034-\003 \037Kedalaman:\037 $Kedalaman \0034-\003 \037Waktu:\037 $Tanggal $Jam \0034-\003 \037Potensi:\037 $Potensi"
+ putnow "privmsg $c :\037https://data.bmkg.go.id/DataMKG/TEWS/$Shakemap\037"
 }
 putlog "+++ Info Gempa BMKG TCL Loaded..."
