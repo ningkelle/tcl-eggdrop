7295bf56f5408149cc7cc915842de06e45c28b61
Ningkelle <halo@ningkelle.id>
2023-11-03 18:13:36 +0700

    Update uptimerobot.tcl

--- uptimerobot.tcl
+++ uptimerobot.tcl
@@ -1,3 +1,4 @@
+# .ruptime / .ruptime <monitors id>
 bind pub - .ruptime pub:ruptime
 proc pub:ruptime {n u h c t} {
  set monitorid [lindex [stripcodes bcruag $t] 0]
@@ -8,20 +9,10 @@ proc pub:ruptime {n u h c t} {
  if {[dict get $response stat] == "fail"} {putnow "privmsg $c :[dict get $response error message]" ; return}
  set monitors [dict get $response monitors] ; if {![llength $monitors] == 1} {putnow "privmsg $c :Not Found" ; return}
  foreach line $monitors {
-  set fname [dict get $line friendly_name]
-  set status [dict get $line status]
-  if {$status == "0"} {set status "Paused"}
-  if {$status == "1"} {set status "not checked yet"}
-  if {$status == "2"} {set status "Up"}
-  if {$status == "8"} {set status "seems down"}
-  if {$status == "9"} {set status "Down"}
-  set id [dict get $line id]
-  set host [dict get $line url]
-  set dtime [dict get $line create_datetime]
-  set idtime [clock format $dtime -format "%d %b %Y %H:%M:%S %Z" -timezone :Asia/Jakarta]
-  set uptime [dict get $line custom_uptime_ratio]
-  set items "\n$fname ($id) \0034-\003 \037Status:\037 $status \0034-\003 \037Uptime:\037 $uptime\% \0034-\003 \037Created:\037 $idtime"
-  append result $items
+  set fname [dict get $line friendly_name] ; set status [dict get $line status]
+  if {$status == "0"} {set status "Paused"} ; if {$status == "1"} {set status "not checked yet"} ; if {$status == "2"} {set status "Up"} ; if {$status == "8"} {set status "seems down"} ; if {$status == "9"} {set status "Down"}
+  set id [dict get $line id] ; set host [dict get $line url] ; set dtime [dict get $line create_datetime] ; set idtime [clock format $dtime -format "%d %b %Y %H:%M:%S %Z" -timezone :Asia/Jakarta] ; set uptime [dict get $line custom_uptime_ratio]
+  set items "\n$fname ($id) \0034-\003 \037Status:\037 $status \0034-\003 \037Uptime:\037 $uptime\% \0034-\003 \037Created:\037 $idtime" ; append result $items
  }
  foreach output [split $result "\n"] {putnow "privmsg $c :$output"}
 }
