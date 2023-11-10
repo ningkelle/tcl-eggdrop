35a2b68be865def41083d04637db6ec2b24b4225
Ningkelle <halo@ningkelle.id>
2023-11-10 12:28:27 +0700

    Update uptimerobot.tcl

--- uptimerobot.tcl
+++ uptimerobot.tcl
@@ -6,10 +6,11 @@ proc pub:ruptime {n u h c t} {
  if {[dict get $response stat] == "fail"} {putnow "privmsg $c :[dict get $response error message]" ; return}
  set monitors [dict get $response monitors] ; if {![llength $monitors] == 1} {putnow "privmsg $c :Not Found" ; return}
  foreach line $monitors {
-  set fname [dict get $line friendly_name] ; set status [dict get $line status]
-  if {$status == "0"} {set status "Paused"} ; if {$status == "1"} {set status "not checked yet"} ; if {$status == "2"} {set status "Up"} ; if {$status == "8"} {set status "seems down"} ; if {$status == "9"} {set status "Down"}
-  set id [dict get $line id] ; set host [dict get $line url] ; set dtime [dict get $line create_datetime] ; set idtime [clock format $dtime -format "%d %b %Y %H:%M:%S %Z" -timezone :Asia/Jakarta] ; set uptime [dict get $line custom_uptime_ratio]
-  set items "\n$fname ($id) \0034-\003 \037Status:\037 $status \0034-\003 \037Uptime:\037 $uptime\% \0034-\003 \037Created:\037 $idtime" ; append result $items
+  set fname [dict get $line friendly_name] ; set id [dict get $line id]
+  set type [dict get $line type] ; if {$type == "1"} {set type "HTTP\(s\)"} ; if {$type == "2"} {set type "Keyword"} ; if {$type == "3"} {set type "Ping"} ; if {$type == "4"} {set type "Port"} ; if {$type == "5"} {set type "Heartbeat"}
+  set status [dict get $line status] ; if {$status == "0"} {set status "Paused"} ; if {$status == "1"} {set status "not checked yet"} ; if {$status == "2"} {set status "Up"} ; if {$status == "8"} {set status "seems down"} ; if {$status == "9"} {set status "Down"}
+  set host [dict get $line url] ; set dtime [dict get $line create_datetime] ; set idtime [clock format $dtime -format "%d %b %Y" -timezone :Asia/Jakarta] ; set uptime [expr {round([dict get $line custom_uptime_ratio])}]
+  set items "\n$fname \0034»»\003 \037Status:\037 $status \0034-\003 \037Uptime:\037 $uptime\% \0034-\003 \037Type:\037 $type \0034-\003 \037Created:\037 $idtime" ; append result $items
  }
  foreach output [split $result "\n"] {putnow "privmsg $c :$output"}
 }
