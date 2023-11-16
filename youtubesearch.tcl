63ff9de99cb2c9e1cc018cae9484cd842c241f30
Ningkelle <halo@ningkelle.id>
2023-11-16 09:45:54 +0700

    Update youtubesearch.tcl

--- youtubesearch.tcl
+++ youtubesearch.tcl
@@ -1,7 +1,6 @@
 bind pub - .yt pub:ytsearch
 proc pub:ytsearch {n u h c t} {
- set t [stripcodes bcruag $t]
- if {$t == ""} {putnow "privmsg $c :Usage: .yt <keyword>" ; return 0}
+ set t [stripcodes bcruag $t] ; if {$t == ""} {putnow "privmsg $c :Usage: .yt <keyword>" ; return 0}
  set apikey "1234567890ABCDEFGHIJKLMNOPQRSTUVWQYZ" ; #YouTube Data API v3 : https://developers.google.com/youtube/v3/getting-started
  if {[catch {set ytpage [http::geturl "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&regionCode=id&safeSearch=none&type=video&key=$apikey&[http::formatQuery q $t]" -timeout 60000]} error]} {putnow "privmsg $c :$error" ; return}
  set response [::json::json2dict [http::data $ytpage]] ; http::cleanup $ytpage
