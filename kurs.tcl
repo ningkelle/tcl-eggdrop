c98a2a7898bc10f9757aab676a23acfe33618a3c
Ningkelle <halo@ningkelle.id>
2024-04-16 09:14:10 +0700

    Update kurs.tcl

--- kurs.tcl
+++ kurs.tcl
@@ -1,6 +1,6 @@
 bind pub - .kurs pub:exchange
 proc pub:exchange {n u h c t} {
- set t [string toupper [stripcodes bcruag $t]]; if {$t == ""} {putnow "privmsg $c :.kurs <mata-uang> <mata-uang-target> <jumlah> \| .kurs kwd idr 5"; return}
+ set t [string toupper [stripcodes bcruag $t]]; if {$t == ""} {putnow "privmsg $c :.kurs <mata-uang> <mata-uang-target> <jumlah> \0034-\003 .kurs kwd idr 5"; return}
  set from [lindex [split $t] 0]; set to [lindex [split $t] 1]; set value [lindex [split $t] 2]; set kursdef "IDR"; set output ""
  if {[regexp {^[0-9]} $to]} {
   set value $to; set kurs ""; if {$kurs == ""} {set to $kursdef} else {set to $kurs}
@@ -11,6 +11,7 @@ proc pub:exchange {n u h c t} {
   if {![regexp {^[0-9]} $value]} {putnow "privmsg $c :\0034ERROR:\003 format salah. Contoh: .kurs kwd idr 5"; return}
   if {[regexp {[,]} $value]} {putnow "privmsg $c :\0034ERROR:\003 format salah. Contoh: .kurs kwd idr 5.21"; return}
  } else {set value 1}
+ if {$value == "0"} {putnow "privmsg $c :Apa kamu tidak punya uang?"; return}
  if {[regexp {^[0-9]} $from]} {putnow "privmsg $c :\0034ERROR:\003 format salah. Contoh: .kurs kwd idr 5"; return}
  if {[string equal -nocase $to $from]} {putnow "privmsg $c :\0034ERROR:\003 format salah. Contoh: .kurs kwd idr 5"; return}
  set apikey "1234567890ABCDEFGHIJKLMNOPQRSTUVWQYZ"; # apikey : www.fixer.io
