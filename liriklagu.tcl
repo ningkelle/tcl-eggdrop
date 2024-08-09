afa090188c70b0a8e273792b3490aa334304afb4
Ningkelle <halo@ningkelle.id>
2024-08-09 15:43:59 +0700

    Create liriklagu.tcl

---
+++ liriklagu.tcl
@@ -0,0 +1,10 @@
+bind pub - .lirik pub:lirik
+proc pub:lirik {n u h c t} {
+ set t [stripcodes bcruag $t]; if {$t == ""} {putnow "privmsg $c :.lirik <judul lagu>"; return}; regsub -all {\s+} $t "%20" text;
+ catch {exec curl -X GET --connect-timeout 5 https://lyrist.vercel.app/api/$text} lirikdata; set lirikjson [json::json2dict $lirikdata]
+ if {![dict exists $lirikjson lyrics]} {putnow "privmsg $c :Not Found"; return}
+ foreach var {lyrics title artist image} {set $var [dict get $lirikjson $var]}
+ putnow "privmsg $c :\037\00353Lirik Lagu:\003\037 $title \0034-\003 \002$artist\002" 
+ foreach line [split $lyrics "\n"] {putnow "privmsg $c :$line"}
+}
+putlog "+++ Lirik Lagu TCL Loaded..."
