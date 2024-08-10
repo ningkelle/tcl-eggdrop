c22997ee042158861b3fbe77649aedff262375c7
Ningkelle <halo@ningkelle.id>
2024-08-10 11:48:01 +0700

    Update wikipedia.tcl

--- wikipedia.tcl
+++ wikipedia.tcl
@@ -1,7 +1,7 @@
 bind pub - .wiki pub:wiki
 proc pub:wiki {n u h c t} {
- set t [stripcodes bcruag $t]; if {$t == ""} {putnow "privmsg $c :Usage: .wiki <kode-bahasa> <keyword>"; return}
- set targetlang [lindex $t 0]; set wikisearch [lrange $t 1 end]
+ set t [stripcodes bcruag $t]; set targetlang [lindex $t 0]; set wikisearch [lrange $t 1 end]
+ if {$t == "" || $wikisearch == ""} {putnow "privmsg $c :Usage: .wiki <kode-bahasa> <keyword>"; return}
  if {[catch {set wikiurl [http::geturl https://$targetlang.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&[http::formatQuery titles $wikisearch] -timeout 30000]} error]} {putnow "privmsg $c :\0034ERROR:\003 $error"; return}
  set wikidata [http::data $wikiurl]; set response [json::json2dict $wikidata]; http::cleanup $wikiurl
  regexp -all -nocase {"pages":\{"(.*?)":\{"} $wikidata "" pageid; if {$pageid == "-1"} {putnow "privmsg $c :$wikisearch \0034»»\003 Not Found"; return}
