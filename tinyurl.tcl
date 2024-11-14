04ce66f65861561ac605b7b0fa647191295d896c
Ningkelle <halo@ningkelle.id>
2024-11-14 16:08:02 +0700

    Update tinyurl.tcl

--- tinyurl.tcl
+++ tinyurl.tcl
@@ -6,7 +6,7 @@ proc pub:tiny {n u h c t} {
  set apiurl "https://api.tinyurl.com/create?api_token=$apitoken";
  set header "Content-Type: application/json"; set headers "accept: application/json"
  set body "\{\"url\": \"$t\",\"domain\": \"tinyurl.com\",\"description\"\: \"beritadetik\"\}"
- catch {exec curl -X POST $apiurl -H $headers -H $header -d $body } data; putlog $data;
+ catch {exec curl -X POST $apiurl -H $headers -H $header -d $body 2> /dev/null} data; putlog $data;
  regexp {"domain":"(.*?)","alias":"(.*?)","} $data x tiny short
  if {![info exists tiny]} {putquick "privmsg $c :\0034ERROR\003"; return} 
  putquick "privmsg $c :https://$tiny/$short"
