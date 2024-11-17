afc0350d27c5651ff8faad8475f614283fad8112
Ningkelle <halo@ningkelle.id>
2024-11-17 11:26:08 +0700

    Update infogempa.tcl

--- infogempa.tcl
+++ infogempa.tcl
@@ -2,7 +2,7 @@ bind pub - .gempa pub:gempa
 proc pub:gempa {n u h c t} {
  if {[catch {set gempaurl [http::geturl https://data.bmkg.go.id/DataMKG/TEWS/autogempa.json -timeout 3000]} error]} {putnow "privmsg $c :$error" ; return}
  set response [::json::json2dict [http::data $gempaurl]] ; http::cleanup $gempaurl ; putlog "$response"
- set gempa [dict get $response Infogempa gempa] ; foreach data {Tanggal Jam Lintang Bujur Magnitude Kedalaman Wilayah Potensi Dirasakan Shakemap} {set $data [dict get $gempa $data]}
+ set gempa [dict get $response Infogempa gempa] ; foreach data {Tanggal Jam Coordinates Lintang Bujur Magnitude Kedalaman Wilayah Potensi Dirasakan Shakemap} {set $data [dict get $gempa $data]}
  putnow "privmsg $c :\00353\037Info Gempa BMKG:\037\003 $Tanggal $Jam \0034-\003 $Potensi"
  putnow "privmsg $c :\037Lokasi:\037 $Wilayah \0034-\003 \037https://google.com/maps?q=$Coordinates\037 \0034-\003 $Dirasakan"
  putnow "privmsg $c :\037Skala:\037 $Magnitude Magnitudo \0034-\003 \037Kedalaman:\037 $Kedalaman \0034-\003 \037https://static.bmkg.go.id/$Shakemap\037"
