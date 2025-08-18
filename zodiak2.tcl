fb69ef3095c1cf3f759288e61808d837e65f1fd3
Ningkelle <halo@ningkelle.id>
2025-08-18 08:14:05 +0700

    Update zodiak.tcl

--- zodiak2.tcl
+++ zodiak2.tcl
@@ -2,17 +2,18 @@ bind pub - .zodiak pub:zdkdtk
 proc pub:zdkdtk {n u h c t} {
  set t [lindex [stripcodes bcruag $t] 0]; set t [string tolower $t]; if {$t == ""} {putquick "privmsg $c :.zdk <zodiak>"; return}
  catch {exec curl --connect-timeout 5 --max-time 60 --retry 5 --retry-delay 2 --retry-max-time 30 -X GET https://wolipop.detik.com/zodiak/$t 2>/dev/null} list;
- regsub -all "\n" $list "" data; regsub -all "</div>" $data "" data; regsub -all "&nbsp;" $data "" data; regsub -all "</strong>" $data "" data; regsub -all "&ndash;" $data " - " data;
- regexp {Peruntungan: (.*?)<br />} $data x peruntungan; regexp {s Zodiac(.*?)<br />} $data x peruntungan2
+ regsub -all "\n" $list "" data; regsub -all "</div>" $data "" data; regsub -all "&nbsp;" $data "" data; regsub -all "</strong>" $data "" data; regsub -all "<strong>" $data "" data; regsub -all "&ndash;" $data " - " data;
+ regexp {Peruntungan: (.*?)<} $data x peruntungan; regexp {s Zodiac(.*?)<} $data x peruntungan2
  if {![info exists peruntungan]} {
   if {[info exists peruntungan2]} {
    set peruntungan $peruntungan2
   } else {putquick "privmsg $c :\0034ERROR:\003 No data found"; return}
  }
- regexp {Keuangan: ([^<]+)<br />} $data x keuangan
- regexp {Asmara: (.*?)<br />} $data x asmara
+ regexp {Keuangan: ([^<]+)<} $data x keuangan
+ regexp {Asmara: (.*?)<} $data x asmara
  regexp {Jam Baik: (.*?)</p>} $data x jambaik; regexp {Jam Baik: (.*?)<div} $data x jambaik2;
  set peruntungan [newline $peruntungan]; set keuangan [newline $keuangan]; set asmara [newline $asmara];
+ putquick "privmsg $c :\002\00309Zodiak: [string toupper $t]\003\002";
  putquick "privmsg $c :\002\00353Peruntungan :\003\002";
  foreach untung $peruntungan {if {$untung != ""} {putquick "privmsg $c :$untung"}}
  putquick "privmsg $c :\002\00353Keuangan :\003\002";
