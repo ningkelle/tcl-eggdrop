f1eca8c94af0ea225ac8eaff2a630504404ab2c6
Ningkelle <halo@ningkelle.id>
2025-08-18 07:59:11 +0700

    Update liriklagu.tcl

--- liriklagu.tcl
+++ liriklagu.tcl
@@ -1,10 +1,11 @@
 bind pub - .lirik pub:lirik
 proc pub:lirik {n u h c t} {
- set t [stripcodes bcruag $t]; if {$t == ""} {putnow "privmsg $c :.lirik <judul lagu>"; return}; regsub -all {\s+} $t "%20" text;
- catch {exec curl -X GET --connect-timeout 5 https://lyrist.vercel.app/api/$text} lirikdata; set lirikjson [json::json2dict $lirikdata]
- if {![dict exists $lirikjson lyrics]} {putnow "privmsg $c :Not Found"; return}
- foreach var {lyrics title artist image} {set $var [dict get $lirikjson $var]}
- putnow "privmsg $c :\037\00353Lirik Lagu:\003\037 $title \0034-\003 \002$artist\002" 
- foreach line [split $lyrics "\n"] {regexp -all {\[(.*?)\]} $line "" verse; if {[info exists verse]} {regsub -all {\[.*?\]} $line "\00363\[$verse\]\003" line}; putnow "privmsg $c :$line"}
+ set t [stripcodes bcruag $t]; if {$t == ""} {putnow "privmsg $c :.lirik <judul lagu>"; return}; regsub -all {\s+} $t "%20" text
+ catch {exec curl -X GET --connect-timeout 5 --max-time 60 --retry 5 --retry-delay 2 --retry-max-time 30 https://lyrics.lewdhutao.my.eu.org/v2/youtube/lyrics?title=$text 2>/dev/null} lirikdata;
+ set lirikjson [json::json2dict $lirikdata]; set response [dict get $lirikjson data];
+ if {[dict exists $response respone]} {set message [dict get $response message]; set error [dict get $response respone]; putnow "privmsg $c :$error \00304»\003 $message"; return}
+ foreach var {artistName trackName trackId lyrics} {set $var [dict get $response $var]}
+ putquick "privmsg $c :\037\00353Lirik Lagu:\003\037 $trackName \0034-\003 \002$artistName\002 \0034-\003 https://youtu.be/$trackId" 
+ foreach line [split $lyrics "\n"] {putquick "privmsg $c :$line"}
 }
 putlog "+++ Lirik Lagu TCL Loaded..."
