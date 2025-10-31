4095bb15894cdd83827d488a141354832a32744e
Ningkelle <halo@ningkelle.id>
2025-10-31 11:14:35 +0700

    Update zodiak.tcl to fetch data from new URL

--- zodiak.tcl
+++ zodiak.tcl
@@ -1,20 +1,17 @@
-bind pub - .zodiak pub:zdkdtk
+set cmds ". ! ` -"
+foreach cmd $cmds {bind pub - ${cmd}zodiak pub:zdkdtk}
 proc pub:zdkdtk {n u h c t} {
  set t [lindex [stripcodes bcruag $t] 0]; set t [string tolower $t]; if {$t == ""} {putquick "privmsg $c :.zdk <zodiak>"; return}
- catch {exec curl --connect-timeout 5 --max-time 60 --retry 5 --retry-delay 2 --retry-max-time 30 -X GET https://wolipop.detik.com/zodiak/$t 2>/dev/null} list;
- regsub -all "\n" $list "" data; regsub -all "</div>" $data "" data; regsub -all "&nbsp;" $data "" data; regsub -all "</strong>" $data "" data;
- regsub -all "<strong>" $data "" data; regsub -all "&ndash;" $data " - " data;
- regexp {Peruntungan:(.*?)<} $data x peruntungan; regexp {s Zodiac(.*?)<} $data x peruntungan2
- if {![info exists peruntungan]} {if {[info exists peruntungan2]} {set peruntungan $peruntungan2} else {putquick "privmsg $c :\0034ERROR:\003 No data found"; return}}
- regexp {Keuangan: ([^<]+)<} $data x keuangan
- regexp {Asmara: (.*?)<} $data x asmara
- regexp {Jam.*?: (.*?)</p>} $data x jambaik; regexp {Jam.*?: (.*?)<div} $data x jambaik2;
- putquick "privmsg $c :\002\00309Zodiak: [string toupper $t]\003\002";
- putquick "privmsg $c :\002\00353Peruntungan :\003\002 $peruntungan";
- putquick "privmsg $c :\002\00353Keuangan :\003\002 $keuangan";
- putquick "privmsg $c :\002\00353Asmara :\003\002 $asmara";
- if {![info exists jambaik]} {if {[info exists jambaik2]} {set jambaik $jambaik2} else {return}}
- regsub -all "<br />" $jambaik "" jambaik;
- putquick "privmsg $c :\002\00353Jam Baik :\003\002 $jambaik";
+ catch {exec curl --connect-timeout 5 --max-time 60 --retry 5 --retry-delay 2 --retry-max-time 30 -X GET https://www.fimela.com/zodiak/$t 2>/dev/null} data;
+ regexp {<span class="zodiak--content-header__date">(.*?)</span>.*Diperbarui </span>(.*?)</time>} $data x tanggal update;
+ regexp {Profil</h5></div><div class="zodiak--content__content"><p>(.*?)</p>} $data x profile;
+ if {![info exists profile]} {pub:zdkdtk $n $c $t; return}
+ regexp {Kesehatan</h5>.*<p>([^<]+)</p></div>.*Love</h5>.*Single: ([^<]+)<br><br>Couple: ([^<]+)</p></div></div>.*Karir</h5>.*<p>([^<]+)</p></div></div>.*Keuangan</h5>.*<p>([^<]+)<} $data x kesehatan single couple karir uang
+ putquick "privmsg $c :\002\00363[string toupper $t]\003\002 ($tanggal) \0034-\003 Update: $update";
+ # set profile [newline $profile]; foreach pro $profile {if {$pro != ""} {putquick "privmsg $c :$pro"}}
+ putquick "privmsg $c :\002\00353Kesehatan :\003\002 $kesehatan";
+ putquick "privmsg $c :\002\00353Love :\003\002 \002\037Single :\037\002 $single \002\037Couple :\037\002 $couple";
+ putquick "privmsg $c :\002\00353Karir :\003\002 $karir";
+ putquick "privmsg $c :\002\00353Keuangan :\003\002 $uang";
 }
 putlog "+++ Zodiak TCL Loaded..."
