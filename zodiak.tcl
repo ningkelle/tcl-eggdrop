67668cd697538b01477f134b4d00798922fec08e
Ningkelle <halo@ningkelle.id>
2025-11-05 09:52:26 +0700

    Update zodiak.tcl

--- zodiak.tcl
+++ zodiak.tcl
@@ -5,7 +5,7 @@ proc pub:zdkdtk {n u h c t} {
  catch {exec curl --connect-timeout 5 --max-time 60 --retry 5 --retry-delay 2 --retry-max-time 30 -X GET https://www.fimela.com/zodiak/$t 2>/dev/null} data;
  regexp {<span class="zodiak--content-header__date">(.*?)</span>.*Diperbarui </span>(.*?)</time>} $data x tanggal update;
  regexp {Profil</h5></div><div class="zodiak--content__content"><p>(.*?)</p>} $data x profile;
- if {![info exists profile]} {pub:zdkdtk $n $c $t; return}
+ if {![info exists profile]} {putquick "privmsg $c :\00304ERROR:\003 No data found"; return}
  regexp {Kesehatan</h5>.*<p>([^<]+)</p></div>.*Love</h5>.*Single: ([^<]+)<br><br>Couple: ([^<]+)</p></div></div>.*Karir</h5>.*<p>([^<]+)</p></div></div>.*Keuangan</h5>.*<p>([^<]+)<} $data x kesehatan single couple karir uang
  putquick "privmsg $c :\002\00363[string toupper $t]\003\002 ($tanggal) \0034-\003 Update: $update";
  # set profile [newline $profile]; foreach pro $profile {if {$pro != ""} {putquick "privmsg $c :$pro"}}
