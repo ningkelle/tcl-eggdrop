bf3bf6e857a392526bdeaf87fb4a4156d5d3011e
Ningkelle <halo@ningkelle.id>
2025-12-18 09:05:20 +0700

    Update serbaserbiserbu.tcl

--- serbaserbiserbu.tcl
+++ serbaserbiserbu.tcl
@@ -5,12 +5,13 @@ package require Tcl
 package require http
 package require tls
 package require json
-# package require tdom 0.9.3
-# package require eggdrop 1.9
+package require textutil
+# package require tdom
+# package require eggdrop
 http::register https 443 [list ::tls::socket -tls1 1 -ssl2 0 -ssl3 0 -autoservername 1]
-http::config -proxyhost "" -useragent "IRCCloud/4.35 (SM-F766B; id; Android 16; 1080x2520; WIFI)"
+http::config -proxyhost "" -useragent "IRCCloud/4.32.1 (iPhone; en-ID; iOS 18.6.2)"
 proc formattedTXT {text} {regsub -all {\003([0-9]{1,2}(,[0-9]{1,2})?)?} $text "" text; regsub -all {[\002\037\026\035\017]} $text "" text; return $text}
 proc commify {n {s ,} {g 3}} {regsub -all \\d(?=(\\d{$g})+([regexp -inline {\.\d*$} $n]$)) $n \\0$s}
 proc noquote {t} {regsub -all {\."} $t {} t ; regsub -all {"}   $t {} t ; return $t}
 proc capitalize {sentence} {subst -nobackslashes -novariables [regsub -all {\S+} $sentence {[string totitle &]}]}
-proc newline {input} {set j 0; set tempdef ""; foreach line [split $input \n] {if {$line != ""} {set len 350 ; set splitChr " "; set out [set cur {}]; set i 0; foreach word [split $line $splitChr] {if {[incr i [string len $word]]>$len} {lappend out [join $cur $splitChr]; set cur [list $word]; set i [string len $word]; incr j} else {lappend cur $word}; incr i}; lappend out [join $cur $splitChr]; foreach line2 $out {if {$j >= 1} {set line2 [linsert $line2 end]; set j [expr $j -1]}; lappend tempdef $line2}}}; return $tempdef}
+proc newline {input} {return [split [textutil::adjust $input -length 350] \n]}
